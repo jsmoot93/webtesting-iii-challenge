e2613559cf0fa6cd39abfbb87521fe57671eb9fb
jsmoot93 <47538435+jsmoot93@users.noreply.github.com>
2019-05-15 17:33:55 -0400

    #1

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,22 @@
-// Test away
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+
+import Dashboard from "./Dashboard";
+
+afterEach(cleanup);
+
+describe("Dashboard Tests", () => {
+  it("Should match snapshot", () => {
+    const { container } = render(<Dashboard />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it("renders successfully without crashing", () => {
+    render(<Dashboard />);
+  });
+  it("should render controls and display", () => {
+    render(<Dashboard />);
+
+    expect(document.querySelector(".controls")).toBeTruthy();
+    expect(document.querySelector(".display")).toBeTruthy();
+  });
+});
